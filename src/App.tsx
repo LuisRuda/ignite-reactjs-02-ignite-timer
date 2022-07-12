276250f0b062e6942ef2cddb5c11a73cbbdee3b3
Luis Paulo Ruda <luispauloruda@gmail.com>
2022-07-12 15:40:17 -0300

    feature: configgured eslint from @rocketseat

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import { ThemeProvider } from 'styled-components';
+import { ThemeProvider } from 'styled-components'
 
-import { GloblaStyle } from './styles/global';
-import { defaultTheme } from './styles/themes/default';
+import { GloblaStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
 
 export function App() {
   return (
