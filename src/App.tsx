b8ba0e519d8208daa21c6a6f54cfc60ebb43cd25
Luis Paulo Ruda <luispauloruda@gmail.com>
2022-07-12 22:51:17 -0300

    feature: added routes with react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import { ThemeProvider } from 'styled-components'
+import { BrowserRouter } from 'react-router-dom'
 
+import { Router } from './Router'
 import { GloblaStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <h1>Ignite Timer</h1>
-
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GloblaStyle />
     </ThemeProvider>
   )
